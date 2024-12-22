363a81a8fd0ea6ac5e15de904771510d013db926
Travis Ennis <travis@travisennis.com>
2024-12-21 21:23:57 -0500

    feat: add ability to set path for messages file

--- source/middleware/auditMessage.ts
+++ source/middleware/auditMessage.ts
@@ -1,58 +1,60 @@
 import fs from "node:fs";
-import {
+import type {
   Experimental_LanguageModelV1Middleware as LanguageModelV1Middleware,
   LanguageModelV1StreamPart,
 } from "ai";
 
-export const auditMessage: LanguageModelV1Middleware = {
-  wrapGenerate: async ({ doGenerate, params }) => {
-    console.log("audit message middleware");
-    const result = await doGenerate();
+export const auditMessage = ({ path = "messages.jsonl" }: { path: string }) => {
+  const middleware: LanguageModelV1Middleware = {
+    wrapGenerate: async ({ doGenerate, params }) => {
+      const result = await doGenerate();
 
-    const msg = {
-      prompt: params.prompt,
-      response: result.text,
-    };
+      const msg = {
+        prompt: params.prompt,
+        response: result.text,
+      };
 
-    const writeStream = fs.createWriteStream("messages.jsonl", { flags: "a" });
-    writeStream.write(`${JSON.stringify(msg)}\n`);
+      const writeStream = fs.createWriteStream(path, { flags: "a" });
+      writeStream.write(`${JSON.stringify(msg)}\n`);
 
-    return result;
-  },
+      return result;
+    },
 
-  wrapStream: async ({ doStream, params }) => {
-    const { stream, ...rest } = await doStream();
+    wrapStream: async ({ doStream, params }) => {
+      const { stream, ...rest } = await doStream();
 
-    let generatedText = "";
+      let generatedText = "";
 
-    const transformStream = new TransformStream<
-      LanguageModelV1StreamPart,
-      LanguageModelV1StreamPart
-    >({
-      transform(chunk, controller) {
-        if (chunk.type === "text-delta") {
-          generatedText += chunk.textDelta;
-        }
+      const transformStream = new TransformStream<
+        LanguageModelV1StreamPart,
+        LanguageModelV1StreamPart
+      >({
+        transform(chunk, controller) {
+          if (chunk.type === "text-delta") {
+            generatedText += chunk.textDelta;
+          }
 
-        controller.enqueue(chunk);
-      },
+          controller.enqueue(chunk);
+        },
 
-      flush() {
-        const msg = {
-          prompt: params.prompt,
-          response: generatedText,
-        };
+        flush() {
+          const msg = {
+            prompt: params.prompt,
+            response: generatedText,
+          };
 
-        const writeStream = fs.createWriteStream("messages.jsonl", {
-          flags: "a",
-        });
-        writeStream.write(`${JSON.stringify(msg)}\n`);
-      },
-    });
+          const writeStream = fs.createWriteStream("messages.jsonl", {
+            flags: "a",
+          });
+          writeStream.write(`${JSON.stringify(msg)}\n`);
+        },
+      });
 
-    return {
-      stream: stream.pipeThrough(transformStream),
-      ...rest,
-    };
-  },
+      return {
+        stream: stream.pipeThrough(transformStream),
+        ...rest,
+      };
+    },
+  };
+  return middleware;
 };
