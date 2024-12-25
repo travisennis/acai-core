43d2357258ecdfb3be7537be064ea5d1d25aca08
Travis Ennis <travis@travisennis.com>
2024-12-25 13:14:19 -0500

    refactor: update how library exports are done

--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,2 @@
-export * as middleware from "./middleware/index.ts";
-export * as optim from "./optim/index.ts";
-export * as tools from "./tools/index.ts";
 export * from "./providers.ts";
-export * from "./providerTypes.ts";
 export * from "./wrapLanguageModel.ts";
-export * from "./mcp.ts";
