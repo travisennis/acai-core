b93170f18b6630e314231d33b2985c8fc1349d2b
Travis Ennis <travis@travisennis.com>
2025-02-18 14:37:28 -0500

    feat: add additional prompt utils

--- source/promptUtils.ts
+++ source/promptUtils.ts
@@ -2,3 +2,11 @@ export function extractXml(text: string, tag: string): string {
   const match = text.match(new RegExp(`<${tag}>(.*?)</${tag}>`, "s"));
   return match ? (match[1] ?? "") : "";
 }
+
+export function removeAllLineBreaks(text: string) {
+  return text.replace(/(\r\n|\n|\r)/gm, " ");
+}
+
+export function removeHtmLtags(text: string) {
+  return text.replace(/<[^>]*>?/gm, "");
+}
