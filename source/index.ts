d609f630b5ca0501bfd4a93048c75c8fa836ec0f
Travis Ennis <travis@travisennis.com>
2025-02-18 17:00:11 -0500

    feat: export new code via index

--- source/index.ts
+++ source/index.ts
@@ -6,4 +6,10 @@ export {
 } from "./providers.ts";
 export { wrapLanguageModel } from "./wrapLanguageModel.ts";
 export { type Dedent, dedent } from "./dedent.ts";
-export { extractXml } from "./promptUtils.ts";
+export {
+  extractXml,
+  removeAllLineBreaks,
+  removeHtmLtags,
+} from "./promptUtils.ts";
+export { ObjectGenerator } from "./objectGenerator.ts";
+export { TokenTracker } from "./tokenTracker.ts";
