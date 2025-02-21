6add00e5891e0d436cf04abf695797641a8f6e46
Travis Ennis <travis@travisennis.com>
2025-02-21 09:09:47 -0500

    feat: add rate limiting middleware

--- source/middleware/index.ts
+++ source/middleware/index.ts
@@ -1,4 +1,6 @@
+// biome-ignore lint/performance/noBarrelFile: <explanation>
 export { usage } from "./usage.ts";
 export { auditMessage } from "./auditMessage.ts";
 export { dbAuditMessage } from "./dbAuditMessage.ts";
 export { log } from "./log.ts";
+export { createRateLimitMiddleware } from "./rateLimit.ts";
