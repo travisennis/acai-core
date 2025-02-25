996bbba5a8f5cdfaf2e6937118cc4c8a4c582aa4
Travis Ennis <travis@travisennis.com>
2025-02-25 12:38:16 -0500

    fix: remove circular export

--- source/tools/index.ts
+++ source/tools/index.ts
@@ -3,7 +3,6 @@ export * from "./codeTools.ts";
 export * from "./filesystem.ts";
 export * from "./git.ts";
 export * from "./hypothesisTesting.ts";
-export * from "./index.ts";
 export * from "./memory.ts";
 export * from "./raindrop.ts";
 export * from "./sequentialThinking.ts";
