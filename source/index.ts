18912fc01429a70fc2dcb00c668a7c87cc78c333
Travis Ennis <travis@travisennis.com>
2025-02-27 10:47:26 -0500

    feat: add utils for managing messages

--- source/index.ts
+++ source/index.ts
@@ -16,3 +16,9 @@ export {
 export { ObjectGenerator } from "./objectGenerator.ts";
 export { TokenTracker } from "./tokenTracker.ts";
 export { analyzeTypeScriptFile, outputFileStructure } from "./codeMap.ts";
+export {
+  createUserMessage,
+  createAssistantMessage,
+  appendResponseMessages,
+  MessageHistory,
+} from "./messages.ts";
